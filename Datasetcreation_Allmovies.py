30f645ebe70ce97e0289141c438555ccef609470
chengshengchang <aa0920758313@gmail.com>
2022-07-22 15:27:43 +0800

    Subtask -> 1. Convert runningtime into interger -> done

--- Datasetcreation_Allmovies.py
+++ Datasetcreation_Allmovies.py
@@ -1,9 +1,11 @@
 from distutils.log import info
+from re import L
 from tkinter.ttk import Style
 from bs4 import BeautifulSoup as bs
 from numpy import tile
 import requests
 import json
+import pandas as pd
 
 
 # Save and Reload data (json format)
@@ -27,6 +29,7 @@ def get_content_value(row_data):
 def clean_tags(soup):
     for tags in soup.find_all(['span','sup']):
         tags.decompose()
+
 
 def get_info_box(url):
     r = requests.get(url)
@@ -72,10 +75,10 @@ base_url = 'https://en.wikipedia.org'
 movie_info_list = []
 
 for index,row in enumerate(movie_info):
+    # not run again cuz I already save to json file
+    break
     if index % 10 == 0:
         print('Progress => ', index)
-    # not run again cuz I already save to json file
-    # break
     try:
         ref_path = row['href']
         full_url = base_url + ref_path
@@ -89,18 +92,44 @@ for index,row in enumerate(movie_info):
 
 
 
-save_data('movie_info_list.json', movie_info_list)
+# save_data('movie_info_list.json', movie_info_list)
 
 
 
 # clean our data
-load_movie_info_list = load_data('movie_info_list.json')
+movie_info_list = load_data('movie_info_list.json')
 
 # Subtasks
 # Clean up Reference [1] [2] -> revise def get_info_box(): -> done
 # Split up the long strings -> done
 # Clean up NoneType and Error wiki page -> done
-# Convert running time into an integer
+
+
+
+# Convert running time into an integer -> done
+
+df_movie_info_list = pd.DataFrame.from_dict(movie_info_list)
+
+
+# input running time will be str
+def running_time_to_int(runningtime):
+    # Case 2
+    if isinstance(runningtime , list):
+        return int(runningtime[0].split(' ')[0])
+    else:
+        entry = str(runningtime).split(' ')[0]
+        if '-' in entry:
+            entry = entry[:2]
+        return  entry
+
+for index , row in enumerate(df_movie_info_list['Running time']):
+    if type(row) != float:
+        df_movie_info_list.loc[index,'Movie minutes (int)']= running_time_to_int(row)
+    else:
+        df_movie_info_list.loc[index,'Movie minutes (int)'] = None
+
+
+# print(df_movie_info_list['Movie minutes (int)'])
 # Convert Dates into datatime object
 # Convert Budget & Box office to numbers
 
